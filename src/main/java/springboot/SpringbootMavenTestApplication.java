4f5a508c588069f551749cd771bcaa178540c681
Mr-LiuDC <442823529@qq.com>
2018-03-09 14:07:18 +0800

    initial project springboot-maven-test

---
+++ src/main/java/springboot/SpringbootMavenTestApplication.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMavenTestApplication.class, args);
+	}
+}
